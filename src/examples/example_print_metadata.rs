474b5a24b6f064241d988e0ecf15663f00e830e1
clangenbacher <christian.langenbacher@scs.ch>
2019-08-08 12:42:36 +0200

    added example_print_metadata

---
+++ src/examples/example_print_metadata.rs
@@ -0,0 +1,44 @@
+/*
+   Copyright 2019 Supercomputing Systems AG
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+#[macro_use]
+extern crate clap;
+extern crate env_logger;
+extern crate substrate_api_client;
+
+use clap::App;
+
+use substrate_api_client::Api;
+use substrate_api_client::node_metadata;
+
+fn main() {
+    env_logger::init();
+
+    let yml = load_yaml!("../../src/examples/cli.yml");
+    let matches = App::from_yaml(yml).get_matches();
+
+    let node_ip = matches.value_of("node-server").unwrap_or("127.0.0.1");
+    let node_port = matches.value_of("node-port").unwrap_or("9944");
+    let url = format!("{}:{}", node_ip, node_port);
+    println!("Interacting with node on {}", url);
+
+    let mut api = Api::new(format!("ws://{}", url));
+    api.init();
+
+    let meta = api.get_metadata().unwrap();
+    println!("Metadata:\n {}", node_metadata::pretty_format(&meta).unwrap());
+}
