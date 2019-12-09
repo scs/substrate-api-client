fdabcb3becb5430d48b4cea9217e8fd5e4d0eaf7
Christian Langenbacher <christian.langenbacher@scs.ch>
2019-12-09 10:34:50 +0100

    simplified node_metadata

--- src/lib.rs
+++ src/lib.rs
@@ -86,7 +86,7 @@ where
         info!("Got genesis hash: {:?}", genesis_hash);
 
         let meta = Self::_get_metadata(url.clone());
-        let metadata = node_metadata::parse_metadata(&meta);
+        let metadata = node_metadata::parse_metadata(meta).unwrap();
         info!("Metadata: {:?}", metadata);
 
         let runtime_version = Self::_get_runtime_version(url.clone());
--- src/node_metadata.rs
+++ src/node_metadata.rs
@@ -15,9 +15,16 @@
 
 */
 
+use std::{
+    collections::HashMap,
+    convert::TryFrom,
+    marker::PhantomData,
+    str::FromStr,
+};
+
 use codec::alloc::string::FromUtf8Error;
 use log::{debug, info};
-use metadata::{DecodeDifferent, RuntimeMetadata, RuntimeMetadataPrefixed};
+use metadata::{DecodeDifferent, RuntimeMetadata, RuntimeMetadataPrefixed, EventMetadata};
 use serde::{Deserialize, Serialize};
 
 pub fn pretty_format(metadata: &RuntimeMetadataPrefixed) -> Result<String, FromUtf8Error> {
@@ -53,7 +60,7 @@ impl Print for NodeMetadata {
 pub struct Module {
     pub name: String,
     pub calls: Vec<Call>,
-    pub events: Vec<Event>,
+    pub events: HashMap<u8, Event>,
 }
 
 impl Module {
@@ -61,7 +68,7 @@ impl Module {
         Module {
             name: format!("{:?}", name).replace("\"", ""),
             calls: Vec::<Call>::new(),
-            events: Vec::<Event>::new(),
+            events: HashMap::new(),
         }
     }
 
@@ -107,16 +114,100 @@ impl Call {
 pub struct Event {
     pub name: String,
     // in this case the only the argument types are provided as strings
-    pub args: Vec<String>,
+    pub arguments: Vec<EventArg>,
+}
+
+impl Event {
+    pub fn arguments(&self) -> Vec<EventArg> {
+        self.arguments.to_vec()
+    }
 }
 
 impl Event {
     fn new(name: &DecodeDifferent<&'static str, std::string::String>) -> Event {
-        Event {
-            name: format!("{:?}", name).replace("\"", ""),
-            args: Vec::<String>::new(),
+        Event { name: format!("{:?}", name).replace("\"", ""), arguments: Vec::<EventArg>::new() }
+    }
+}
+
+/// Naive representation of event argument types, supports current set of substrate EventArg types.
+/// If and when Substrate uses `type-metadata`, this can be replaced.
+///
+/// Used to calculate the size of a instance of an event variant without having the concrete type,
+/// so the raw bytes can be extracted from the encoded `Vec<EventRecord<E>>` (without `E` defined).
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum EventArg {
+    Primitive(String),
+    Vec(Box<EventArg>),
+    Tuple(Vec<EventArg>),
+}
+
+impl FromStr for EventArg {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("Vec<") {
+            if s.ends_with('>') {
+                Ok(EventArg::Vec(Box::new(s[4..s.len() - 1].parse()?)))
+            } else {
+                Err(Error::InvalidEventArg(
+                    s.to_string(),
+                    "Expected closing `>` for `Vec`",
+                ))
+            }
+        } else if s.starts_with('(') {
+            if s.ends_with(')') {
+                let mut args = Vec::new();
+                for arg in s[1..s.len() - 1].split(',') {
+                    let arg = arg.trim().parse()?;
+                    args.push(arg)
+                }
+                Ok(EventArg::Tuple(args))
+            } else {
+                Err(Error::InvalidEventArg(
+                    s.to_string(),
+                    "Expecting closing `)` for tuple",
+                ))
+            }
+        } else {
+            Ok(EventArg::Primitive(s.to_string()))
         }
     }
+}
+
+impl EventArg {
+    /// Returns all primitive types for this EventArg
+    pub fn primitives(&self) -> Vec<String> {
+        match self {
+            EventArg::Primitive(p) => vec![p.clone()],
+            EventArg::Vec(arg) => arg.primitives(),
+            EventArg::Tuple(args) => {
+                let mut primitives = Vec::new();
+                for arg in args {
+                    primitives.extend(arg.primitives())
+                }
+                primitives
+            }
+        }
+    }
+}
+
+fn convert<B: 'static, O: 'static>(dd: DecodeDifferent<B, O>) -> Result<O, Error> {
+    match dd {
+        DecodeDifferent::Decoded(value) => Ok(value),
+        _ => Err(Error::ExpectedDecoded),
+    }
+}
+
+fn convert_event(
+    event: EventMetadata,
+) -> Result<Event, Error> {
+    let name = convert(event.name)?;
+    let mut arguments = Vec::new();
+    for arg in convert(event.arguments)? {
+        let arg = arg.parse::<EventArg>()?;
+        arguments.push(arg);
+    }
+    Ok(Event { name, arguments })
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -137,84 +228,59 @@ impl Arg {
     }
 }
 
-pub fn parse_metadata(metadata: &RuntimeMetadataPrefixed) -> Vec<Module> {
+pub fn parse_metadata(metadata: RuntimeMetadataPrefixed) -> Result<Vec<Module>, Error> {
     let mut mod_vec = Vec::<Module>::new();
-    match &metadata.1 {
-        RuntimeMetadata::V11(value) => {
-            match &value.modules {
-                DecodeDifferent::Decoded(mods) => {
-                    let modules = mods;
-                    debug!("-------------------- modules ----------------");
-                    for module in modules {
-                        debug!("module: {:?}", module.name);
-                        let mut _mod = Module::new(&module.name);
-                        match &module.calls {
-                            Some(DecodeDifferent::Decoded(calls)) => {
-                                debug!("-------------------- calls ----------------");
 
-                                if calls.is_empty() {
-                                    // indices modules does for some reason list `Some([])' as calls and is thus counted in the call enum
-                                    // there might be others doing the same.
-                                    _mod.calls.push(Default::default())
-                                }
+    let meta = match metadata.1 {
+        RuntimeMetadata::V8(meta) => meta,
+        _ => return Err(Error::InvalidVersion),
+    };
+    debug!("-------------------- modules ----------------");
+    for module in convert(meta.modules)?.into_iter() {
+        debug!("module: {:?}", module.name);
+        let mut _mod = Module::new(&module.name);
+        debug!("-------------------- calls ----------------");
+        if let Some(calls) = module.calls {
+            let calls = convert(calls)?;
 
-                                for call in calls {
-                                    let mut _call = Call::new(&call.name);
-                                    match &call.arguments {
-                                        DecodeDifferent::Decoded(arguments) => {
-                                            for arg in arguments {
-                                                _call.args.push(Arg::new(&arg.name, &arg.ty));
-                                            }
-                                        }
-                                        _ => unreachable!(
-                                            "All calls have at least the 'who' argument; qed"
-                                        ),
-                                    }
-                                    _mod.calls.push(_call);
-                                }
-                            }
-                            _ => debug!("No calls for this module"),
-                        }
+            if calls.is_empty() {
+                // indices modules does for some reason list `Some([])' as calls and is thus counted in the call enum
+                // there might be others doing the same.
+                _mod.calls.push(Default::default())
+            }
 
-                        match &module.event {
-                            Some(DecodeDifferent::Decoded(event)) => {
-                                debug!("-------------------- events ----------------");
-                                debug!("{:?}", event);
-                                if event.is_empty() {
-                                    // indices modules does for some reason list `Some([])' as calls and is thus counted in the call enum
-                                    // there might be others doing the same.
-                                    _mod.calls.push(Default::default())
-                                }
-
-                                for e in event {
-                                    let mut _event = Event::new(&e.name);
-                                    match &e.arguments {
-                                        DecodeDifferent::Decoded(arguments) => {
-                                            for arg in arguments {
-                                                _event.args.push(arg.to_string());
-                                            }
-                                        }
-                                        _ => unreachable!(
-                                            "All calls have at least the 'who' argument; qed"
-                                        ),
-                                    }
-                                    _mod.events.push(_event);
-                                }
-                            }
-                            _ => debug!("No calls for this module"),
-                        }
-
-                        mod_vec.push(_mod);
-                    }
-                    for m in &mod_vec {
-                        info!("{:?}", m);
-                    }
-                    debug!("successfully decoded metadata");
+            for call in calls.into_iter() {
+                let mut _call = Call::new(&call.name);
+                for arg in convert(call.arguments)?.into_iter() {
+                    _call.args.push(Arg::new(&arg.name, &arg.ty));
                 }
-                _ => unreachable!("There are always modules present; qed"),
+                _mod.calls.push(_call);
             }
         }
-        _ => panic!("Unsupported metadata"),
+
+        if let Some(events) = module.event {
+            let mut event_map = HashMap::new();
+            for (index, e) in convert(events)?.into_iter().enumerate() {
+                event_map.insert(index as u8, convert_event(e).unwrap());
+            }
+            _mod.events = event_map;
+        } else {
+            debug!("No calls for this module");
+        }
+
+        mod_vec.push(_mod);
     }
-    mod_vec
+    for m in &mod_vec {
+        info!("{:?}", m);
+    }
+    debug!("successfully decoded metadata");
+    Ok(mod_vec)
+}
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidPrefix,
+    InvalidVersion,
+    ExpectedDecoded,
+    InvalidEventArg(String, &'static str),
 }
