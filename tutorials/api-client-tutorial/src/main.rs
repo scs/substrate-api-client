808a3ccb94c319ae7c0837c0bd3c1390cc4d71ad
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-05-24 14:25:18 +0200

    fix clippy warnings

--- tutorials/api-client-tutorial/src/main.rs
+++ tutorials/api-client-tutorial/src/main.rs
@@ -38,7 +38,7 @@ fn main() {
         .unwrap();
 
     let xt: UncheckedExtrinsicV4<_> =
-        compose_extrinsic!(api, "KittyModule", "create_kitty", 10 as u128);
+        compose_extrinsic!(api, "KittyModule", "create_kitty", 10_u128);
 
     println!("[+] Extrinsic: {:?}\n", xt);
 
