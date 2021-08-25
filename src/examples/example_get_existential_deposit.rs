fb175e0e22e506163b873057a521106e4239f020
echevrier <edith.chevrier@scs.ch>
2021-08-25 14:53:39 +0200

    Implement #129: add constants in metadata and read existential deposit from balances pallet

---
+++ src/examples/example_get_existential_deposit.rs
@@ -0,0 +1,44 @@
+/*
+Copyright 2019 Supercomputing Systems AG
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+///! Very simple example that shows how to get some simple storage values.
+use clap::{load_yaml, App};
+
+use substrate_api_client::rpc::WsRpcClient;
+use substrate_api_client::sp_runtime::app_crypto::sp_core::sr25519;
+use substrate_api_client::Api;
+
+fn main() {
+    env_logger::init();
+    let url = get_node_url_from_cli();
+
+    let client = WsRpcClient::new(&url);
+    let api = Api::<sr25519::Pair, _>::new(client).unwrap();
+
+    // get existential deposit
+    let min_balance = api.get_existential_deposit().unwrap();
+    println!("[+] Existential Deposit is {}", min_balance);
+}
+
+pub fn get_node_url_from_cli() -> String {
+    let yml = load_yaml!("../../src/examples/cli.yml");
+    let matches = App::from_yaml(yml).get_matches();
+
+    let node_ip = matches.value_of("node-server").unwrap_or("ws://127.0.0.1");
+    let node_port = matches.value_of("node-port").unwrap_or("9944");
+    let url = format!("{}:{}", node_ip, node_port);
+    println!("Interacting with node on {}\n", url);
+    url
+}
