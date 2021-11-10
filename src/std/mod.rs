7c59970a2a7d8dd2c7abf3539987d016bcee2321
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-11-10 13:00:11 +0100

    remove example_custom_client but add the code to the documentation.

--- src/std/mod.rs
+++ src/std/mod.rs
@@ -40,6 +40,64 @@ pub trait RpcClient {
     fn send_extrinsic(&self, xthex_prefixed: String, exit_on: XtStatus) -> ApiResult<Option<Hash>>;
 }
 
+/// Api to talk with substrate-nodes
+///
+/// It is generic over the `RpcClient` trait, so you can use any rpc-backend you like.
+///
+/// # Custom Client Example
+///
+/// ```no_run
+/// use substrate_api_client::rpc::json_req::author_submit_extrinsic;
+/// use substrate_api_client::{
+///     Api, ApiClientError, ApiResult, FromHexString, Hash, RpcClient, Value, XtStatus,
+/// };
+/// struct MyClient {
+///     // pick any request crate, such as ureq::Agent
+///     _inner: (),
+/// }
+///
+/// impl MyClient {
+///     pub fn new() -> Self {
+///         Self {
+///             // ureq::agent()
+///             _inner: (),
+///         }
+///     }
+///
+///     pub fn send_json<R>(
+///         &self,
+///         _path: String,
+///         _json: Value,
+///     ) -> Result<R, Box<dyn std::error::Error>> {
+///         // you can figure this out...self.inner...send_json...
+///         todo!()
+///     }
+/// }
+///
+/// impl RpcClient for MyClient {
+///     fn get_request(&self, jsonreq: serde_json::Value) -> ApiResult<String> {
+///         self.send_json::<Value>("".into(), jsonreq)
+///             .map(|v| v.to_string())
+///             .map_err(|err| ApiClientError::RpcClient(err.to_string()))
+///     }
+///
+///     fn send_extrinsic(
+///         &self,
+///         xthex_prefixed: String,
+///         _exit_on: XtStatus,
+///     ) -> ApiResult<Option<Hash>> {
+///         let jsonreq = author_submit_extrinsic(&xthex_prefixed);
+///         let res: String = self
+///             .send_json("".into(), jsonreq)
+///             .map_err(|err| ApiClientError::RpcClient(err.to_string()))?;
+///         Ok(Some(Hash::from_hex(res)?))
+///     }
+/// }
+///
+/// let client = MyClient::new();
+/// let _api = Api::<(), _>::new(client);
+///
+/// ```
 #[derive(Clone)]
 pub struct Api<P, Client>
 where
