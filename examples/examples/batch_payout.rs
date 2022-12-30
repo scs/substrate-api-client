a5797f44972c16b20041e6622fedb05c6844f6da
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2022-12-30 13:46:28 +0100

    some more clean up

--- examples/examples/batch_payout.rs
+++ examples/examples/batch_payout.rs
@@ -21,6 +21,8 @@ use substrate_api_client::{
 	rpc::JsonrpseeClient, Api, GetStorage, PlainTipExtrinsicParams, SubmitAndWatch, XtStatus,
 };
 
+const MAX_BATCHED_TRANSACTION: u32 = 9;
+
 pub struct GracePeriod {
 	pub enabled: bool,
 	pub eras: u32,
@@ -47,49 +49,52 @@ async fn main() {
 	let mut api = Api::<_, _, PlainTipExtrinsicParams<Runtime>, Runtime>::new(client).unwrap();
 	api.set_signer(alice);
 
-	// Give a valid validator account address, given one is westend chain validator account
+	// Give a valid validator account address, given one is westend chain validator account.
 	let account =
 		AccountId32::from_ss58check("5DJcEbkNxsnNwHGrseg7cgbfUG8eiKzpuZqgSph5HqHrjgf6").unwrap();
 
 	let active_era: ActiveEraInfo =
 		api.get_storage_value("Staking", "ActiveEra", None).unwrap().unwrap();
+	println!("{:?}", active_era);
+
 	let mut last_reward = get_last_reward(&account, &api);
-	let max_batched_transactions = 9;
 	let current_active_era = active_era.index;
-	let mut num_of_unclaimed_payout = current_active_era - last_reward - 1;
-	let mut start = 1;
-	let mut num_of_claimed_payouts = 0;
 	let grace_period = GracePeriod { enabled: false, eras: 0 };
 
-	let mut results: Vec<Value> = Vec::new();
-	while num_of_unclaimed_payout > 0 {
-		let mut payout_calls = vec![];
-		let mut tx_limit = num_of_unclaimed_payout;
-		if num_of_unclaimed_payout > max_batched_transactions {
-			tx_limit = max_batched_transactions;
-		}
+	let mut num_of_unclaimed_payouts = current_active_era - last_reward - 1;
+	let mut num_of_claimed_payouts = 0;
+	let mut results = Vec::<Value>::new();
+	let mut start = 1;
+	while num_of_unclaimed_payouts > 0 {
+		let tx_limit = if num_of_unclaimed_payouts > MAX_BATCHED_TRANSACTION {
+			MAX_BATCHED_TRANSACTION
+		} else {
+			num_of_unclaimed_payouts
+		};
 
+		let mut payout_calls = vec![];
 		let mut i = start;
 		while i < tx_limit + start {
 			let idx = last_reward + i;
 			let is_grace_period_satisfied =
 				!grace_period.enabled || (current_active_era - idx > grace_period.eras);
-			let mut exposure: Exposure<AccountId32, u128> =
-				Exposure { total: 0, own: 0, others: vec![] };
 
-			if let Some(exp) = api
+			let exposure: Exposure<AccountId32, u128> = match api
 				.get_storage_double_map("Staking", "ErasStakers", idx, &account, None)
 				.unwrap()
 			{
-				exposure = exp
-			}
+				Some(exposure) => exposure,
+				None => Exposure { total: 0, own: 0, others: vec![] },
+			};
+
 			if exposure.total.to_be_bytes() > 0_u128.to_be_bytes() && is_grace_period_satisfied {
-				let some = api.payout_stakers(idx, account.clone());
-				payout_calls.push(some.function);
+				let payout_extrinsic = api.payout_stakers(idx, account.clone());
+				payout_calls.push(payout_extrinsic.function);
 			}
 			i += 1;
 			last_reward += 1;
 		}
+
 		let payout_calls_len = payout_calls.len();
 		if payout_calls_len > 0 {
 			let batching = api.batch(payout_calls);
@@ -103,7 +108,7 @@ async fn main() {
 			let result = serde_json::to_value(results_hash).unwrap();
 			results.push(result);
 		}
-		num_of_unclaimed_payout -= tx_limit;
+		num_of_unclaimed_payouts -= tx_limit;
 		start += tx_limit;
 	}
 	println!("{:?}", results);
@@ -121,26 +126,21 @@ pub fn get_last_reward(
 ) -> u32 {
 	let active_era: ActiveEraInfo =
 		api.get_storage_value("Staking", "ActiveEra", None).unwrap().unwrap();
-	let storagekey = api.metadata().storage_map_key("Staking", "Ledger", account).unwrap();
-	let mut res = StakingLedger {
-		stash: account.clone(),
-		total: 0,
-		active: 0,
-		unlocking: Vec::new(),
-		claimed_rewards: Vec::new(),
-	};
+	let storage_key = api.metadata().storage_map_key("Staking", "Ledger", account).unwrap();
 
-	if let Ok(Some(ledger)) = api.get_storage_by_key_hash(storagekey, None) {
-		res = ledger
-	}
+	let claimed_rewards: Vec<u32> =
+		match api.get_storage_by_key_hash::<StakingLedger>(storage_key, None) {
+			Ok(Some(ledger)) => ledger.claimed_rewards,
+			_ => Vec::new(),
+		};
 
 	let is_history_checked_force = false;
 
-	let last_reward = if is_history_checked_force || res.claimed_rewards.is_empty() {
+	let last_reward = if is_history_checked_force || claimed_rewards.is_empty() {
 		let history_depth: u32 = api.get_constant("Staking", "HistoryDepth").unwrap();
 		active_era.index - history_depth
 	} else {
-		res.claimed_rewards.pop().unwrap()
+		*claimed_rewards.last().unwrap()
 	};
 	println!("{}", last_reward);
 	last_reward
