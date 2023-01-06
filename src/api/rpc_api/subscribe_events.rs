7b327d7dbd8ebb2a80d6d26baef479d0060e7787
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2023-01-06 11:46:15 +0100

    remove wait_for_event_details and remove return upon error event

--- src/api/rpc_api/subscribe_events.rs
+++ src/api/rpc_api/subscribe_events.rs
@@ -15,29 +15,21 @@ use crate::{
 	api::{error::Error, Api, Result},
 	rpc::{HandleSubscription, Subscribe},
 };
-use ac_node_api::{events::EventDetails, Events, StaticEvent};
+use ac_node_api::{Events, StaticEvent};
 use ac_primitives::{ExtrinsicParams, FrameSystemConfig, StorageChangeSet};
 use log::*;
 use serde::de::DeserializeOwned;
-
-// FIXME: This should rather be implemented directly on the
-// Subscription return value, rather than the api. Or directly
-// subcribe. Should be looked at in #288
-// https://github.com/scs/substrate-api-client/issues/288#issuecomment-1346221653
 pub trait SubscribeEvents<Client, Hash>
 where
 	Client: Subscribe,
 	Hash: DeserializeOwned,
 {
+	/// Listens for a specific event type and only returns if an undecodeable
+	/// Event is received or the event has been found.
 	fn wait_for_event<Ev: StaticEvent>(
 		&self,
 		subscription: &mut Client::Subscription<StorageChangeSet<Hash>>,
 	) -> Result<Ev>;
-
-	fn wait_for_event_details<Ev: StaticEvent>(
-		&self,
-		subscription: &mut Client::Subscription<StorageChangeSet<Hash>>,
-	) -> Result<EventDetails>;
 }
 
 impl<Signer, Client, Params, Runtime> SubscribeEvents<Client, Runtime::Hash>
@@ -51,16 +43,6 @@ where
 		&self,
 		subscription: &mut Client::Subscription<StorageChangeSet<Runtime::Hash>>,
 	) -> Result<Ev> {
-		let maybe_event_details = self.wait_for_event_details::<Ev>(subscription)?;
-		maybe_event_details
-			.as_event()?
-			.ok_or(Error::Other("Could not find the specific event".into()))
-	}
-
-	fn wait_for_event_details<Ev: StaticEvent>(
-		&self,
-		subscription: &mut Client::Subscription<StorageChangeSet<Runtime::Hash>>,
-	) -> Result<EventDetails> {
 		while let Some(change_set) = subscription.next() {
 			let event_bytes = change_set?.changes[0].1.as_ref().unwrap().0.clone();
 			let events = Events::<Runtime::Hash>::new(
@@ -71,21 +53,16 @@ where
 			for maybe_event_details in events.iter() {
 				let event_details = maybe_event_details?;
 
-				// Check for failed xt and return as Dispatch Error in case we find one.
-				// Careful - this reports the first one encountered. This event may belong to another extrinsic
-				// than the one that is being waited for.
-				event_details.check_if_failed()?;
-
-				let event_metadata = event_details.event_metadata();
-				trace!(
-					"Found extrinsic: {:?}, {:?}",
-					event_metadata.pallet(),
-					event_metadata.event()
-				);
-				if event_metadata.pallet() == Ev::PALLET && event_metadata.event() == Ev::EVENT {
-					return Ok(event_details)
-				} else {
-					trace!("Not the event we are looking for, skipping.")
+				match event_details.as_event::<Ev>()? {
+					Some(event) => return Ok(event),
+					None => {
+						trace!(
+							"Found extrinsic: {:?}, {:?}",
+							event_details.event_metadata().pallet(),
+							event_details.event_metadata().event()
+						);
+						trace!("Not the event we are looking for, skipping.");
+					},
 				}
 			}
 		}
