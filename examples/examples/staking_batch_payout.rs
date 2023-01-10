d88e199d9f9d81a2d9babb30072e60e20b46b820
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2023-01-10 17:04:47 +0100

    make it CI compatible and simplify

--- examples/examples/staking_batch_payout.rs
+++ examples/examples/staking_batch_payout.rs
@@ -14,15 +14,16 @@
 use codec::{Decode, Encode};
 use kitchensink_runtime::{Runtime, Signature};
 use pallet_staking::{ActiveEraInfo, Exposure};
-use serde_json::Value;
 use sp_keyring::AccountKeyring;
 use sp_runtime::{app_crypto::Ss58Codec, AccountId32};
 use substrate_api_client::{
 	rpc::JsonrpseeClient, Api, ExtrinsicSigner, GetStorage, PlainTipExtrinsicParams,
-	SubmitAndWatch, XtStatus,
+	SubmitAndWatch, SubmitAndWatchUntilSuccess, XtStatus,
 };
 
 const MAX_BATCHED_TRANSACTION: u32 = 9;
+
+pub type EraIndex = u32;
 
 pub struct GracePeriod {
 	pub enabled: bool,
@@ -50,99 +51,125 @@ async fn main() {
 	let mut api = Api::<_, _, PlainTipExtrinsicParams<Runtime>, Runtime>::new(client).unwrap();
 	api.set_signer(ExtrinsicSigner::<_, Signature, Runtime>::new(alice));
 
-	// Give a valid validator account address, given one is westend chain validator account.
-	let account =
-		AccountId32::from_ss58check("5DJcEbkNxsnNwHGrseg7cgbfUG8eiKzpuZqgSph5HqHrjgf6").unwrap();
+	// Give a valid validator account address. In the kitchinsink runtime, this is Alice.
+	let validator_account = AccountKeyring::Alice.to_account_id();
+	// Alice Stash:
+	let validator_stash =
+		AccountId32::from_ss58check("5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY").unwrap();
 
 	let active_era: ActiveEraInfo =
 		api.get_storage_value("Staking", "ActiveEra", None).unwrap().unwrap();
 	println!("{:?}", active_era);
+	let current_era_index = active_era.index;
+	// Test if payout staker extrinsic works. Careful: In CI, this extrinsic will fail to be executed, because
+	// one can not payout the current era (= 0 on the freshly started node). But this is okay, because we know if this
+	// error is returned from the node, the extrinsic has been created correctly.
+	// Sidenote: We could theoretically force a new era with sudo, but this takes at least 10 minutes ( = 1 epoch) in the
+	// kitchensink rutime. We don't want to wait that long.
+	let payout_staker_xt = api.payout_stakers(0, validator_stash);
+	let result = api.submit_and_watch_extrinsic_until_success(payout_staker_xt, false);
+	assert!(result.is_err());
+	assert!(format!("{result:?}").contains("InvalidEraToReward"));
 
-	let mut last_reward = get_last_reward(&account, &api);
-	let current_active_era = active_era.index;
-	let grace_period = GracePeriod { enabled: false, eras: 0 };
-
-	let mut num_of_unclaimed_payouts = current_active_era - last_reward - 1;
-	let mut num_of_claimed_payouts = 0;
-	let mut results = Vec::<Value>::new();
-	let mut start = 1;
-	while num_of_unclaimed_payouts > 0 {
-		let tx_limit = if num_of_unclaimed_payouts > MAX_BATCHED_TRANSACTION {
-			MAX_BATCHED_TRANSACTION
-		} else {
-			num_of_unclaimed_payouts
-		};
-
-		let mut payout_calls = vec![];
-		let mut i = start;
-		while i < tx_limit + start {
-			let idx = last_reward + i;
-			let is_grace_period_satisfied =
-				!grace_period.enabled || (current_active_era - idx > grace_period.eras);
-
-			let exposure: Exposure<AccountId32, u128> = match api
-				.get_storage_double_map("Staking", "ErasStakers", idx, &account, None)
-				.unwrap()
-			{
-				Some(exposure) => exposure,
-				None => Exposure { total: 0, own: 0, others: vec![] },
+	// From here on, this is not CI tested, but it serves as a nice example nonetheless.
+	if let Some(mut last_reward_received_at_era) =
+		get_last_reward_received_for(&validator_account, current_era_index, &api)
+	{
+		let grace_period = GracePeriod { enabled: false, eras: 0 };
+		let mut num_of_unclaimed_payouts: u32 =
+			if current_era_index - last_reward_received_at_era > 0 {
+				current_era_index - last_reward_received_at_era - 1
+			} else {
+				0
+			};
+		let mut num_of_claimed_payouts = 0;
+		let mut results = Vec::new();
+		while num_of_unclaimed_payouts > 0 {
+			let tx_limit_in_current_batch = if num_of_unclaimed_payouts > MAX_BATCHED_TRANSACTION {
+				MAX_BATCHED_TRANSACTION
+			} else {
+				num_of_unclaimed_payouts
 			};
 
-			if exposure.total.to_be_bytes() > 0_u128.to_be_bytes() && is_grace_period_satisfied {
-				let payout_extrinsic = api.payout_stakers(idx, account.clone());
-				payout_calls.push(payout_extrinsic.function);
+			// Get all payout extrinsic for the unclaimed era that fit in the current batch.
+			let mut payout_calls = vec![];
+			let mut i = 0;
+			while i < tx_limit_in_current_batch {
+				let payout_era_index = last_reward_received_at_era + i;
+				let is_grace_period_satisfied = !grace_period.enabled
+					|| (current_era_index - payout_era_index > grace_period.eras);
+
+				let exposure: Exposure<AccountId32, u128> = match api
+					.get_storage_double_map(
+						"Staking",
+						"ErasStakers",
+						payout_era_index,
+						&validator_account,
+						None,
+					)
+					.unwrap()
+				{
+					Some(exposure) => exposure,
+					None => Exposure { total: 0, own: 0, others: vec![] },
+				};
+
+				if exposure.total.to_be_bytes() > 0_u128.to_be_bytes() && is_grace_period_satisfied
+				{
+					let payout_extrinsic =
+						api.payout_stakers(payout_era_index, validator_account.clone());
+					payout_calls.push(payout_extrinsic.function);
+				}
+				i += 1;
+				last_reward_received_at_era += 1;
 			}
-			i += 1;
-			last_reward += 1;
-		}
+			num_of_claimed_payouts += payout_calls.len();
+			num_of_unclaimed_payouts -= tx_limit_in_current_batch;
+			let batch_xt = api.batch(payout_calls);
 
-		let payout_calls_len = payout_calls.len();
-		if payout_calls_len > 0 {
-			let batching = api.batch(payout_calls);
-			let results_hash = api
-				.submit_and_watch_extrinsic_until(batching, XtStatus::InBlock)
-				.unwrap()
-				.block_hash
-				.unwrap();
-			num_of_claimed_payouts += payout_calls_len;
-
-			let result = serde_json::to_value(results_hash).unwrap();
-			results.push(result);
+			let report = api.submit_and_watch_extrinsic_until(batch_xt, XtStatus::InBlock).unwrap();
+			results.push(format!("{report:?}"));
 		}
-		num_of_unclaimed_payouts -= tx_limit;
-		start += tx_limit;
-	}
-	println!("{:?}", results);
-	println!("Unclaimed payouts: {:?}", num_of_claimed_payouts);
+		println!("{:?}", results);
+		println!("Unclaimed payouts: {:?}", num_of_claimed_payouts);
+	};
 }
 
-pub fn get_last_reward(
+pub fn get_last_reward_received_for(
 	account: &AccountId32,
+	current_era: EraIndex,
 	api: &substrate_api_client::Api<
 		ExtrinsicSigner<sp_core::sr25519::Pair, Signature, Runtime>,
 		JsonrpseeClient,
-		PlainTipExtrinsicParams<Runtime>,
+		AssetTipExtrinsicParams<Runtime>,
 		Runtime,
 	>,
-) -> u32 {
-	let active_era: ActiveEraInfo =
-		api.get_storage_value("Staking", "ActiveEra", None).unwrap().unwrap();
-	let storage_key = api.metadata().storage_map_key("Staking", "Ledger", account).unwrap();
+) -> Option<u32> {
+	let ledger_storage_key = api.metadata().storage_map_key("Staking", "Ledger", account).unwrap();
 
 	let claimed_rewards: Vec<u32> =
-		match api.get_storage_by_key_hash::<StakingLedger>(storage_key, None) {
+		match api.get_storage_by_key_hash::<StakingLedger>(ledger_storage_key, None) {
 			Ok(Some(ledger)) => ledger.claimed_rewards,
 			_ => Vec::new(),
 		};
 
-	let is_history_checked_force = false;
-
-	let last_reward = if is_history_checked_force || claimed_rewards.is_empty() {
+	// Get the era index the last reward has been retrieved.
+	let last_reward_received_at_era = if claimed_rewards.is_empty() {
 		let history_depth: u32 = api.get_constant("Staking", "HistoryDepth").unwrap();
-		active_era.index - history_depth
+		// Ensure we don't get below zero here.
+		if current_era > history_depth {
+			let last_known_era = current_era - history_depth;
+			Some(last_known_era)
+		} else {
+			None // The caller most likely has never received any rewards yet.
+		}
 	} else {
-		*claimed_rewards.last().unwrap()
+		claimed_rewards.last().copied()
 	};
-	println!("{}", last_reward);
-	last_reward
+	if let Some(reward_retrieved) = last_reward_received_at_era {
+		println!("Retrieved the last reward at era Index {reward_retrieved:?}");
+	} else {
+		println!("{account:?} did not receive any rewards yet");
+	}
+
+	last_reward_received_at_era
 }
