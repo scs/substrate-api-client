2257b2401958443077208a3e5794364274dff94e
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2023-01-13 09:56:36 +0100

    fix clippy

--- testing/examples/frame_system_tests.rs
+++ testing/examples/frame_system_tests.rs
@@ -51,26 +51,26 @@ async fn main() {
 
 	// System Api
 	let system_name = api.get_system_name().unwrap();
-	println!("System name: {}", system_name);
+	println!("System name: {system_name}");
 
 	let system_version = api.get_system_version().unwrap();
-	println!("System version: {}", system_version);
+	println!("System version: {system_version}");
 
 	let system_chain = api.get_system_chain().unwrap();
-	println!("System chain: {}", system_chain);
+	println!("System chain: {system_chain}");
 
 	let system_chain_type = api.get_system_chain_type().unwrap();
-	println!("System chain type: {:?}", system_chain_type);
+	println!("System chain type: {system_chain_type:?}");
 
 	let system_properties = api.get_system_properties().unwrap();
-	println!("System properties: {:?}", system_properties);
+	println!("System properties: {system_properties:?}");
 
 	let system_health = api.get_system_health().unwrap();
-	println!("System health: {}", system_health);
+	println!("System health: {system_health}");
 
 	let system_local_peer_id = api.get_system_local_peer_id().unwrap();
-	println!("System local peer id: {:?}", system_local_peer_id);
+	println!("System local peer id: {system_local_peer_id:?}");
 
 	let system_local_listen_addresses = api.get_system_local_listen_addresses().unwrap();
-	println!("System local listen addresses: {:?}", system_local_listen_addresses);
+	println!("System local listen addresses: {system_local_listen_addresses:?}");
 }
