a4b042795a7e13588d2f2da58dd51b9c352ee4bd
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2023-03-30 18:03:02 +0200

    add static extrinisc signer
    fix signer

--- primitives/src/signer.rs
+++ primitives/src/signer.rs
@@ -20,8 +20,8 @@
 use crate::FrameSystemConfig;
 use codec::{Decode, Encode};
 use core::marker::PhantomData;
-use sp_core::Pair;
-use sp_runtime::traits::StaticLookup;
+use sp_core::{crypto::AccountId32, Pair};
+use sp_runtime::{traits::StaticLookup, MultiAddress};
 
 pub trait SignExtrinsic<AccountId: Clone + Encode> {
 	type Signature: Encode;
@@ -88,5 +88,76 @@ where
 
 	fn extrinsic_address(&self) -> Self::ExtrinsicAddress {
 		self.extrinsic_address.clone()
+	}
+}
+
+#[derive(Encode, Decode, Clone, PartialEq)]
+pub struct StaticExtrinsicSigner<Signer, Signature> {
+	signer: Signer,
+	account_id: AccountId32,
+	extrinsic_address: MultiAddress<AccountId32, u32>,
+	_phantom: PhantomData<Signature>,
+}
+
+impl<Signer, Signature> StaticExtrinsicSigner<Signer, Signature>
+where
+	Signer: Pair,
+	Signer::Public: Into<AccountId32>,
+	Signature: From<Signer::Signature> + Encode + Clone,
+{
+	pub fn new(signer: Signer) -> Self {
+		let account_id = signer.public().into();
+		let extrinsic_address = MultiAddress::from(account_id.clone());
+		Self { signer, account_id, extrinsic_address, _phantom: Default::default() }
+	}
+
+	pub fn signer(&self) -> &Signer {
+		&self.signer
+	}
+}
+
+impl<Signer, Signature> SignExtrinsic<AccountId32> for StaticExtrinsicSigner<Signer, Signature>
+where
+	Signer: Pair,
+	Signature: From<Signer::Signature> + Encode + Clone,
+{
+	type Signature = Signature;
+	type ExtrinsicAddress = MultiAddress<AccountId32, u32>;
+
+	fn sign(&self, payload: &[u8]) -> Self::Signature {
+		self.signer.sign(payload).into()
+	}
+
+	fn public_account_id(&self) -> &AccountId32 {
+		&self.account_id
+	}
+
+	fn extrinsic_address(&self) -> Self::ExtrinsicAddress {
+		self.extrinsic_address.clone()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use node_template_runtime::Signature;
+	use sp_keyring::AccountKeyring;
+	//use node_template_runtime::Runtime;
+
+	/// This test fails!
+	// 	#[test]
+	// 	fn test_extrinsic_signer_clone() {
+	// 		let pair = AccountKeyring::Alice.pair();
+	// 		let signer = ExtrinsicSigner::<_, Signature, Runtime>::new(pair);
+	//
+	// 		let signer2 = signer.clone();
+	// 	}
+
+	#[test]
+	fn test_simple_extrinsic_signer_clone() {
+		let pair = AccountKeyring::Alice.pair();
+		let signer = StaticExtrinsicSigner::<_, Signature>::new(pair);
+
+		let _signer2 = signer.clone();
 	}
 }
