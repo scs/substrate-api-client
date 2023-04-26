03761456577f44d91c05f68b0089949f9d155a5d
Thomas Niederberger <thomas@niederb.ch>
2023-04-26 07:25:59 +0200

    Improve async example

--- examples/examples/get_blocks_async.rs
+++ examples/examples/get_blocks_async.rs
@@ -13,16 +13,13 @@
 	limitations under the License.
 */
 
-//! Very simple example that shows how to pretty print the metadata. Has proven to be a helpful
-//! debugging tool.
+//! Very simple example that shows how to fetch chain metadata with async.
 
 use kitchensink_runtime::Runtime;
 use sp_core::sr25519;
 use std::time::Instant;
 use substrate_api_client::{
-	ac_primitives::PlainTipExtrinsicParams,
-	rpc::{HandleSubscription, JsonrpseeClient},
-	Api, GetChainInfo, SubscribeChain,
+	ac_primitives::PlainTipExtrinsicParams, rpc::JsonrpseeClient, Api, GetChainInfo,
 };
 
 #[tokio::main]
@@ -36,37 +33,38 @@ async fn main() {
 		.await
 		.unwrap();
 
-	println!("Genesis block: \n {:?} \n", api.get_genesis_block().await.unwrap());
-
-	let header_hash = api.get_finalized_head().await.unwrap().unwrap();
+	let (genesis_block, header_hash, signed_block) = futures::future::try_join3(
+		api.get_genesis_block(),
+		api.get_finalized_head(),
+		api.get_finalized_block(),
+	)
+	.await
+	.unwrap();
+	let header_hash = header_hash.unwrap();
+	let signed_block = signed_block.unwrap();
+	println!("Genesis block: \n {:?} \n", genesis_block);
 	println!("Latest Finalized Header Hash:\n {} \n", header_hash);
-
-	let header = api.get_header(Some(header_hash)).await.unwrap().unwrap();
-	println!("Finalized header:\n {:?} \n", header);
-
-	let signed_block = api.get_finalized_block().await.unwrap().unwrap();
-	println!("Finalized block:\n {:?} \n", signed_block);
 
 	let last_block_number = signed_block.block.header.number;
 	// Get the previous three blocks of the last_block_number
 	let number_of_last_three_blocks: Vec<_> =
 		(last_block_number.saturating_sub(3)..last_block_number).collect();
-	let blocks = api.get_signed_blocks(&number_of_last_three_blocks).await.unwrap();
+
+	let (header, blocks, latest_header, latest_block) = futures::future::try_join4(
+		api.get_header(Some(header_hash)),
+		api.get_signed_blocks(&number_of_last_three_blocks),
+		api.get_header(None),
+		api.get_block(None),
+	)
+	.await
+	.unwrap();
+	println!("Finalized header:\n {:?} \n", header.unwrap());
+	println!("Finalized block:\n {:?} \n", signed_block);
 	println!("Block numbers of the previous three blocks: ");
 	for (i, b) in blocks.iter().enumerate() {
 		println!("  Block {} has block number {}", i, b.block.header.number);
 	}
-
-	println!("Latest Header: \n {:?} \n", api.get_header(None).await.unwrap());
-
-	println!("Latest block: \n {:?} \n", api.get_block(None).await.unwrap());
+	println!("Latest Header: \n {:?} \n", latest_header);
+	println!("Latest block: \n {:?} \n", latest_block);
 	println!("Fetching block information took {} ms", start.elapsed().as_millis());
-
-	println!("Subscribing to finalized heads");
-	let mut subscription = api.subscribe_finalized_heads().unwrap();
-
-	for _ in 0..5 {
-		let head = subscription.next().unwrap().unwrap();
-		println!("Got new Block {:?}", head);
-	}
 }
