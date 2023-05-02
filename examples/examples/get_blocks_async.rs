e1c766cb9312cedd40f024d8e4a1d955abc8ab1c
Thomas Niederberger <781000+Niederb@users.noreply.github.com>
2023-05-02 11:20:11 +0200

    Provide an async example (#551)
    * Add initial example that compiles (but breaks the other examples)
    
    * Improve async example
    
    * Adjust features to compile both cases
    
    * Fix taplo formatting
    
    * Try different approach for feature flags
    
    * Add comment
    
    * Incorporate review feedback
    
    * Modify build pipeline to build async example after the rest
    
    * Try different approach in build pipeline for async examples
    
    * Add missing comma
    
    * Add subscription part
    
    * Fix use declarations

--- examples/examples/get_blocks_async.rs
+++ examples/examples/get_blocks_async.rs
@@ -13,8 +13,8 @@
 	limitations under the License.
 */
 
-//! Very simple example that shows how to pretty print the metadata. Has proven to be a helpful
-//! debugging tool.
+//! Very simple example that shows how to fetch chain information with async.
+//! To compile this example for async you need to set the `--no-default-features` flag
 
 use kitchensink_runtime::Runtime;
 use sp_core::sr25519;
@@ -24,43 +24,61 @@ use substrate_api_client::{
 	Api, GetChainInfo, SubscribeChain,
 };
 
+// Empty implementation that is used in case the example is compiled in synchronous mode
+#[cfg(feature = "sync-examples")]
+#[tokio::main]
+async fn main() {
+	println!("Please compile this example with `--no-default-features` for it to run properly.")
+}
+
+#[cfg(not(feature = "sync-examples"))]
 #[tokio::main]
 async fn main() {
 	env_logger::init();
 
 	// Initialize the api.
 	let client = JsonrpseeClient::with_default_url().unwrap();
-	let api =
-		Api::<sr25519::Pair, _, PlainTipExtrinsicParams<Runtime>, Runtime>::new(client).unwrap();
+	let api = Api::<sr25519::Pair, _, PlainTipExtrinsicParams<Runtime>, Runtime>::new(client)
+		.await
+		.unwrap();
 
-	println!("Genesis block: \n {:?} \n", api.get_genesis_block().unwrap());
-
-	let header_hash = api.get_finalized_head().unwrap().unwrap();
+	let (genesis_block, header_hash, signed_block) = futures::future::try_join3(
+		api.get_genesis_block(),
+		api.get_finalized_head(),
+		api.get_finalized_block(),
+	)
+	.await
+	.unwrap();
+	let header_hash = header_hash.unwrap();
+	let signed_block = signed_block.unwrap();
+	println!("Genesis block: \n {:?} \n", genesis_block);
 	println!("Latest Finalized Header Hash:\n {} \n", header_hash);
-
-	let header = api.get_header(Some(header_hash)).unwrap().unwrap();
-	println!("Finalized header:\n {:?} \n", header);
-
-	let signed_block = api.get_finalized_block().unwrap().unwrap();
-	println!("Finalized block:\n {:?} \n", signed_block);
 
 	let last_block_number = signed_block.block.header.number;
 	// Get the previous three blocks of the last_block_number
 	let number_of_last_three_blocks: Vec<_> =
 		(last_block_number.saturating_sub(3)..last_block_number).collect();
-	let blocks = api.get_signed_blocks(&number_of_last_three_blocks).unwrap();
+
+	let (header, blocks, latest_header, latest_block) = futures::future::try_join4(
+		api.get_header(Some(header_hash)),
+		api.get_signed_blocks(&number_of_last_three_blocks),
+		api.get_header(None),
+		api.get_block(None),
+	)
+	.await
+	.unwrap();
+	println!("Finalized header:\n {:?} \n", header.unwrap());
+	println!("Finalized block:\n {:?} \n", signed_block);
 	println!("Block numbers of the previous three blocks: ");
 	for (i, b) in blocks.iter().enumerate() {
 		println!("  Block {} has block number {}", i, b.block.header.number);
 	}
+	println!("Latest Header: \n {:?} \n", latest_header);
+	println!("Latest block: \n {:?} \n", latest_block);
 
-	println!("Latest Header: \n {:?} \n", api.get_header(None).unwrap());
-
-	println!("Latest block: \n {:?} \n", api.get_block(None).unwrap());
-
+	// This part is still executed synchronously
 	println!("Subscribing to finalized heads");
 	let mut subscription = api.subscribe_finalized_heads().unwrap();
-
 	for _ in 0..5 {
 		let head = subscription.next().unwrap().unwrap();
 		println!("Got new Block {:?}", head);
