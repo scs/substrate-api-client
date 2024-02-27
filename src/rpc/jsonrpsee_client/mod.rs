b44ac6bbbed7ac323dc6715a62bf55d3e8a3b94f
Bigna Härdi <Bigna.Haerdi@scs.ch>
2024-02-27 10:32:16 +0100

    Add Jsonrpsee::new_with_client (#735)
    * add access function and generated with new
    
    * add wrapper and test
    
    * add test
    
    * udpate jsonrspee library

--- src/rpc/jsonrpsee_client/mod.rs
+++ src/rpc/jsonrpsee_client/mod.rs
@@ -15,7 +15,7 @@ use crate::rpc::{Error, Request, Result, RpcParams, Subscribe};
 use jsonrpsee::{
 	client_transport::ws::{Url, WsTransportClientBuilder},
 	core::{
-		client::{Client, ClientBuilder, ClientT, SubscriptionClientT},
+		client::{Client, ClientBuilder, ClientT, Error as JsonrpseeError, SubscriptionClientT},
 		traits::ToRpcParams,
 	},
 };
@@ -33,6 +33,7 @@ pub struct JsonrpseeClient {
 }
 
 impl JsonrpseeClient {
+	/// Create a new client to a local Substrate node with default port.
 	pub async fn with_default_url() -> Result<Self> {
 		Self::new("ws://127.0.0.1:9944").await
 	}
@@ -58,6 +59,42 @@ impl JsonrpseeClient {
 	pub async fn new_with_port(address: &str, port: u32) -> Result<Self> {
 		let url = format!("{address}:{port:?}");
 		Self::new(&url).await
+	}
+
+	/// Create a new client with a user-generated Jsonrpsee Client.
+	pub fn new_with_client(client: Client) -> Self {
+		let inner = Arc::new(client);
+		Self { inner }
+	}
+}
+
+impl JsonrpseeClient {
+	/// Checks if the client is connected to the target.
+	pub fn is_connected(&self) -> bool {
+		self.inner.is_connected()
+	}
+
+	/// This is similar to [`Client::on_disconnect`] but it can be used to get
+	/// the reason why the client was disconnected but it's not cancel-safe.
+	///
+	/// The typical use-case is that this method will be called after
+	/// [`Client::on_disconnect`] has returned in a "select loop".
+	///
+	/// # Cancel-safety
+	///
+	/// This method is not cancel-safe
+	pub async fn disconnect_reason(&self) -> JsonrpseeError {
+		self.inner.disconnect_reason().await
+	}
+
+	/// Completes when the client is disconnected or the client's background task encountered an error.
+	/// If the client is already disconnected, the future produced by this method will complete immediately.
+	///
+	/// # Cancel safety
+	///
+	/// This method is cancel safe.
+	pub async fn on_disconnect(&self) {
+		self.inner.on_disconnect().await;
 	}
 }
 
--- testing/async/examples/jsonrpsee_tests.rs
+++ testing/async/examples/jsonrpsee_tests.rs
@@ -15,7 +15,15 @@
 
 //! Tests for the Jsonrpseeclient Wrapper. Should happen during runtime, otherwise no connection can be etablished.
 
+use core::panic;
+
+use jsonrpsee::{
+	client_transport::ws::{Url, WsTransportClientBuilder},
+	core::client::{Client, ClientBuilder},
+	server::{RpcModule, Server},
+};
 use substrate_api_client::rpc::JsonrpseeClient;
+use tokio::{sync::oneshot, task, time, time::Duration};
 
 #[tokio::main]
 async fn main() {
@@ -30,4 +38,51 @@ async fn main() {
 	assert!(client2.is_ok());
 	// No node running at this port - creation should fail.
 	assert!(client3.is_err());
+
+	// Test new_with_client and extra functionality of inner Jsonrpseee client.
+	let (tx, mut rx) = oneshot::channel();
+	let finish_message = "Finishing task";
+
+	// Start server.
+	let server = Server::builder().build("127.0.0.1:0").await.unwrap();
+	let addr = server.local_addr().unwrap();
+	let server_handle = server.start(RpcModule::new(()));
+
+	// Create client and connect.
+	let uri = Url::parse(&format!("ws://{}", addr)).unwrap();
+	let (tx1, rx1) = WsTransportClientBuilder::default().build(uri).await.unwrap();
+	let client: Client = ClientBuilder::default().build_with_tokio(tx1, rx1);
+	let api_rpsee_client = JsonrpseeClient::new_with_client(client);
+	assert!(api_rpsee_client.is_connected());
+
+	let client_handle = task::spawn(async move {
+		println!("Waiting for client disconnect");
+		api_rpsee_client.on_disconnect().await;
+		time::sleep(Duration::from_secs(2)).await;
+		println!("Disconnected due to: {:?}", api_rpsee_client.disconnect_reason().await);
+		tx.send(finish_message).unwrap();
+	});
+
+	// Drop server such that client gets a disconnect.
+	drop(server_handle);
+
+	// Wait for the disconnect message.
+	let timeout = 5;
+	let mut ctr = 0;
+	loop {
+		if let Ok(message) = rx.try_recv() {
+			assert_eq!(finish_message, message);
+			println!("{message}");
+			break;
+		} else {
+			ctr += 1;
+			if ctr == timeout {
+				panic!("Timeout");
+			}
+			time::sleep(Duration::from_secs(1)).await;
+			println!("sleeping..");
+		}
+	}
+
+	client_handle.await.unwrap();
 }
