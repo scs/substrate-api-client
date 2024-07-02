26e8ac6c40ff6aab6a78d28566ee0fd22bf6322e
Thomas Niederberger <thomas@niederb.ch>
2024-07-02 11:28:05 +0200

    Introduce feature to disable the metadata-hash-check

--- primitives/src/extrinsics/extrinsic_params.rs
+++ primitives/src/extrinsics/extrinsic_params.rs
@@ -38,12 +38,16 @@ pub struct GenericSignedExtra<Tip, Index> {
 	#[codec(compact)]
 	pub nonce: Index,
 	pub tip: Tip,
+	#[cfg(feature = "disable-metadata-hash-check")]
 	pub check_hash: u8,
 }
 
 impl<Tip, Index> GenericSignedExtra<Tip, Index> {
 	pub fn new(era: Era, nonce: Index, tip: Tip) -> Self {
+		#[cfg(feature = "disable-metadata-hash-check")]
 		Self { era, nonce, tip, check_hash: 0 }
+		#[cfg(not(feature = "disable-metadata-hash-check"))]
+		Self { era, nonce, tip }
 	}
 }
 
@@ -56,7 +60,10 @@ impl<Tip, Index> GenericSignedExtra<Tip, Index> {
 // defines what is returned upon the `additional_signed` call. The AdditionalSigned defined here
 // must mirror these return values.
 // Example: https://github.com/paritytech/substrate/blob/23bb5a6255bbcd7ce2999044710428bc4a7a924f/frame/system/src/extensions/check_non_zero_sender.rs#L64-L66
+#[cfg(feature = "disable-metadata-hash-check")]
 pub type GenericAdditionalSigned<Hash> = ((), u32, u32, Hash, Hash, (), (), (), Option<[u8; 32]>);
+#[cfg(not(feature = "disable-metadata-hash-check"))]
+pub type GenericAdditionalSigned<Hash> = ((), u32, u32, Hash, Hash, (), (), ());
 
 /// This trait allows you to configure the "signed extra" and
 /// "additional" parameters that are signed and used in substrate extrinsics.
@@ -180,6 +187,7 @@ where
 	}
 
 	fn additional_signed(&self) -> Self::AdditionalSigned {
+		#[cfg(feature = "disable-metadata-hash-check")]
 		(
 			(),
 			self.spec_version,
@@ -190,6 +198,17 @@ where
 			(),
 			(),
 			None,
+		)
+		#[cfg(not(feature = "disable-metadata-hash-check"))]
+		(
+			(),
+			self.spec_version,
+			self.transaction_version,
+			self.genesis_hash,
+			self.mortality_checkpoint,
+			(),
+			(),
+			(),
 		)
 	}
 }
