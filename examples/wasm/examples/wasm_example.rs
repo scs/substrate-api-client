2a4209ec4157acfbc44bf4310f399020160e5a05
Thomas Niederberger <781000+Niederb@users.noreply.github.com>
2024-07-04 11:52:54 +0200

    Wasm test (#772)
    * Scaffolding for wasm test
    
    * Fix command to build wasm example
    
    * State os for running job statically
    
    * Run example with wasmtime
    
    * Try different approach for a build with a wasm test
    
    * Add missing comma
    
    * Fix toml formatting
    
    * Adjust path
    
    * Test a failing example
    
    * Return an Err
    
    * Improve wasm example
    
    * Working on example
    
    * Implement functionality
    
    * Improve example
    
    * Cleanup example
    
    * Revert "Implement functionality"
    
    This reverts commit 69005747926830e37e97af08479d84f51f26c9b1.
    
    * Add test documentation
    
    * Incorporate review feedback

---
+++ examples/wasm/examples/wasm_example.rs
@@ -0,0 +1,50 @@
+/*
+	Copyright 2024 Supercomputing Systems AG
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+//! Example that some basic functions that can be executed in WebAssembly.
+
+pub use pallet_balances::Call as BalancesCall;
+use sp_core::{
+	crypto::{AccountId32, Ss58Codec},
+	sr25519, Pair,
+};
+use sp_runtime::MultiAddress;
+pub use sp_runtime::OpaqueExtrinsic as UncheckedExtrinsic;
+use std::process::ExitCode;
+use substrate_api_client::ac_primitives::{AssetRuntimeConfig, ExtrinsicSigner};
+
+fn main() -> Result<ExitCode, i32> {
+	// This test is not yet very sophisticated and not exhaustive.
+	// Still it shows how some basic data structures can be constructed and used.
+	let alice: sr25519::Pair = Pair::from_string(
+		"0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a",
+		None,
+	)
+	.unwrap();
+
+	let bob_account: AccountId32 =
+		sr25519::Public::from_ss58check("5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty")
+			.unwrap()
+			.into();
+	let _bob: MultiAddress<AccountId32, AccountId32> = MultiAddress::Id(bob_account);
+	let es_converted: ExtrinsicSigner<AssetRuntimeConfig> = alice.clone().into();
+	let es_new = ExtrinsicSigner::<AssetRuntimeConfig>::new(alice.clone());
+	assert_eq!(es_converted.signer().public(), es_new.signer().public());
+
+	let extrinsic = UncheckedExtrinsic::from_bytes(&[]);
+	match extrinsic {
+		Ok(_) => panic!("Extrinsic should be invalid"),
+		Err(_) => (),
+	}
+	Ok(ExitCode::from(0))
+}
