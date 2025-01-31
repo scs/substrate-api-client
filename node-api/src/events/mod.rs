f0d2850a1380e16974a75c7cbf31bd4336e17277
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2025-01-31 09:26:00 +0100

    fix clippy

--- node-api/src/events/mod.rs
+++ node-api/src/events/mod.rs
@@ -126,8 +126,7 @@ impl<Hash: Copy + Encode + Decode> Events<Hash> {
 	/// them, and return only those which should decode to the provided `Ev` type.
 	/// If an error occurs, all subsequent iterations return `None`.
 	pub fn find<Ev: StaticEvent>(&self) -> impl Iterator<Item = Result<Ev, Error>> + '_ {
-		self.iter()
-			.filter_map(|ev| ev.and_then(|ev| ev.as_event::<Ev>().map_err(Into::into)).transpose())
+		self.iter().filter_map(|ev| ev.and_then(|ev| ev.as_event::<Ev>()).transpose())
 	}
 
 	/// Iterate through the events using metadata to dynamically decode and skip
