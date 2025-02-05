42705c44cf3b139618efadd4bc16d6bbe98e3d21
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2025-02-05 11:44:12 +0100

    fix rebase error

--- src/api/runtime_api/transaction_payment.rs
+++ src/api/runtime_api/transaction_payment.rs
@@ -133,7 +133,11 @@ where
 		Address: Encode,
 		Call: Encode,
 		Signature: Encode,
+		TransactionExtension: Encode,
 	{
+		self.query_info_opaque(extrinsic.encode(), length, at_block).await
+	}
+
 	async fn query_info_opaque(
 		&self,
 		extrinsic: Vec<u8>,
